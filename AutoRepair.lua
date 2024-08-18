443fdaa8ea856829489a10eeb81f24a878bf7b63
Lewis Halstead <lewishalstead5@gmail.com>
2024-08-18 17:03:15 +0100

    update

--- AutoRepair.lua
+++ AutoRepair.lua
@@ -1,10 +1,15 @@
-if not AutoRepairDB then 
+if not AutoRepairDB or AutoRepairDB.version ~= '1.1.0' then 
+    local backupPersonalSpend = AutoRepairDB and AutoRepairDB.totalRepairSpendPersonal or 0
+    local backupGuildSpend = AutoRepairDB and AutoRepairDB.totalRepairSpendGuild or 0
+
     AutoRepairDB = {
-        totalRepairSpend = 0,
+        version = '1.2.0',
+        totalRepairSpendPersonal = backupPersonalSpend,
+        totalRepairSpendGuild = backupGuildSpend,
         enabled = true,
-        constToDate = true,
+        costToDate = true,
         guildRepair = false,
-        normalRepair = true
+        personalRepair = true
     }
 end
 
@@ -14,18 +19,40 @@ frame:RegisterEvent("MERCHANT_SHOW")
 print("AutoRepair v1.1.0 loaded")
 print("If you find AutoRepair useful, or want to see additional features, please leave a comment on Curse Forge!")
 
+local function guildRepairFunc(repairCost)
+    RepairAllItems(true)
+    AutoRepairDB.totalRepairSpendGuild = AutoRepairDB.totalRepairSpendGuild + repairCost
+    print("All items repaired for " .. C_CurrencyInfo.GetCoinTextureString(repairCost) .. "(guild)")
+end
+
+local function personalRepairFunc(repairCost)
+    if repairCost > 0 and GetMoney() >= repairCost then
+        RepairAllItems()
+        AutoRepairDB.totalRepairSpendPersonal = AutoRepairDB.totalRepairSpendPersonal + repairCost
+        print("All items repaired for " .. C_CurrencyInfo.GetCoinTextureString(repairCost) .. "(personal)")
+    elseif repairCost > 0 then
+        print("Not enough money to repair items")
+    end
+end
+
+local function printCostToDate()
+    local TotalSpend = C_CurrencyInfo.GetCoinTextureString(AutoRepairDB.totalRepairSpendGuild + AutoRepairDB.totalRepairSpendPersonal) or C_CurrencyInfo.GetCoinTextureString(0)
+    print(string.format("Repair cost to date: %s", TotalSpend))
+end
+
 local function OnEvent(self, event, ...)
-    if event == "MERCHANT_SHOW" and CanMerchantRepair() and AutoRepairDB.enabled then
-        local repairCost = GetRepairAllCost()
-        if repairCost > 0 and GetMoney() >= repairCost then
-            RepairAllItems()
-            AutoRepairDB.totalRepairSpend = AutoRepairDB.totalRepairSpend + repairCost
-            print("All items repaired for " .. C_CurrencyInfo.GetCoinTextureString(repairCost))
-            if AutoRepairDB.constToDate then
-                print(string.format("Repair cost to date: %s", C_CurrencyInfo.GetCoinTextureString(AutoRepairDB.totalRepairSpend)))
-            end
-        elseif repairCost > 0 then
-            print("Not enough money to repair items")
+    local repairCost = GetRepairAllCost()
+    if event == "MERCHANT_SHOW" and CanMerchantRepair() and AutoRepairDB.enabled and repairCost > 0 then
+        local totalRepair = AutoRepairDB.totalRepairSpendGuild + AutoRepairDB.totalRepairSpendPersonal
+
+        if AutoRepairDB.guildRepair then
+            guildRepairFunc(repairCost)
+        elseif AutoRepairDB.personalRepair then
+            personalRepairFunc(repairCost)
+        end
+
+        if AutoRepairDB.costToDate then
+            printCostToDate()
         end
     end
 end
@@ -33,32 +60,84 @@ end
 frame:SetScript("OnEvent", OnEvent)
 
 local function enableAutoRepair()
-    AutoRepairDB.enabled = true
-    print("AutoRepair addon enabled")
+    if AutoRepairDB.enabled == false then
+        AutoRepairDB.enabled = true
+        print("AutoRepair enabled")
+    elseif AutoRepairDB.enabled then
+        print("AutoRepair is already enabled")
+    end
 end
 
 local function disableAutoRepair()
-    AutoRepairDB.enabled = false
-    print("AutoRepair addon disabled")
-end
-
-local function printCostToDate()
-    local TotalSpend = C_CurrencyInfo.GetCoinTextureString(AutoRepairDB.totalRepairSpend)
-    print(string.format("Repair cost to date: %s", TotalSpend))
+    if AutoRepairDB.enabled then
+        AutoRepairDB.enabled = false
+        print("AutoRepair disabled")
+    elseif AutoRepairDB.enabled == false then
+        print("AutoRepair is already disabled")
+    end
 end
 
 local function enableCostToDate()
-    AutoRepairDB.constToDate = true
-    print("Cost-to-date enabled")
+    if AutoRepairDB.costToDate == false then
+        AutoRepairDB.costToDate = true
+        print("Cost-to-date enabled")
+    elseif AutoRepairDB.costToDate then
+        print("Cost-to-date already enabled")
+    end
 end
 
 local function disableCostToDate()
-    AutoRepairDB.constToDate = false
-    print("Cost-to-date disabled")
+    if AutoRepairDB.costToDate then
+        AutoRepairDB.costToDate = false
+        print("Cost-to-date disabled")
+    elseif AutoRepairDB.costToDate == false then
+        print("Cost-to-date already disabled")
+    end
+end
+
+local function enableGuildRepair()
+    if AutoRepairDB.guildRepair == false then
+        AutoRepairDB.guildRepair = true
+        print("Guild repair enabled")
+    elseif AutoRepairDB.guildRepair then
+        print("Guild repair already enabled")
+    end
+end
+
+local function disableGuildRepair()
+    if AutoRepairDB.guildRepair then
+        AutoRepairDB.guildRepair = false
+        print("Guild repair disabled")
+    elseif AutoRepairDB.guildRepair == false then
+        print("Guild repair already disabled")
+    end
+end
+
+-- FIX
+local function enablePersonalRepair()
+    print("Debug: Current personalRepair state: ", AutoRepairDB.personalRepair)
+    print("Debug: Current ctd state: ", AutoRepairDB.costToDate)
+    print("Debug: Current guildRepair state: ", AutoRepairDB.guildRepair)
+    if AutoRepairDB.personalRepair == false then
+        AutoRepairDB.personalRepair = true
+        print("Personal repair enabled")
+    elseif AutoRepairDB.personalRepair then
+        print("Personal repair already enabled")
+    end
+end
+
+local function disablePersonalRepair()
+    print("Debug: Current personalRepair state: ", AutoRepairDB.personalRepair)
+    if AutoRepairDB.personalRepair then
+        AutoRepairDB.personalRepair = false
+        print("Personal repair disabled")
+    elseif AutoRepairDB.personalRepair == false then
+        print("Personal repair already disabled")
+    end
 end
 
 local function showHelp()
-    print("Commands:\nEnable AutoRepair: /autorepair enable\nDisable AutoRepair: /autorepair disable\nEnable cost-to-date: /autorepair ctd enable\nDisable cost-to-date: /autorepair ctd disable\nPrint cost-to-date: /autorepair ctd")
+    print("Commands:\nEnable AutoRepair: /autorepair enable\nDisable AutoRepair: /autorepair disable\nEnable cost-to-date: /autorepair ctd enable\nDisable cost-to-date: /autorepair ctd disable\nPrint cost-to-date: /autorepair ctd\nEnable guild repair: /autorepair guild enable\nDisable guild repair: /autorepair guild disable\nEnable personal repair: /autorepair personal enable\nDisable personal repair: /autorepair personal disable")
 end
 
 local commands = {
@@ -67,11 +146,15 @@ local commands = {
     ["ctd"] = printCostToDate,
     ["ctd enable"] = enableCostToDate,
     ["ctd disable"] = disableCostToDate,
+    ["guild enable"] = enableGuildRepair,
+    ["guild disable"] = disableGuildRepair,
+    ["personal enable"] = enablePersonalRepair,
+    ["personal disable"] = disablePersonalRepair,
     ["help"] = showHelp
 }
 
 local function HandleSlashCommand(input)
-    local command = commands[input]
+    local command = commands[string.lower(input)]
     if command then
         command()
     else
